82020d36681de7363d12380ce18b9338b4b59d66
kareem batisha <kareembatisha39@gmail.com>
2024-06-11 14:41:12 +0300

    fix page

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -3,22 +3,33 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js"; // Correct import
 import CheckoutForm from "./_components/CheckoutForm";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 ); // Correct spelling
-function Page() {
+
+function Checkout() {
   const searchparams = useSearchParams();
+  const amount = Number(searchparams.get("amount"));
   const options = {
     mode: "payment",
     currency: "usd",
-    amount: Number(searchparams.get("amount")) * 100,
+    amount: amount * 100,
   };
   return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={amount} />
+    </Elements>
+  );
+}
+
+function Page() {
+  return (
     <div>
-      <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm amount={Number(searchparams.get("amount"))} />
-      </Elements>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Checkout />
+      </Suspense>
     </div>
   );
 }
